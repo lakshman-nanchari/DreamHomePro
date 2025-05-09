4f3c795d980822128859a37df7eb4275ae30ed1b
lakshman nanchari <lakshmannanchari96@gmail.com>
2025-05-09 20:43:32 +0530

    initial setup

---
+++ dreamhome/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './index.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className='text-3xl text-blue-500 font-bold underline'>hello world</div>
+    </>
+  )
+}
+
+export default App
